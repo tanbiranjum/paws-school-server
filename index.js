9d2592f8b69c05275f516d3012723969e1d60253
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-26 16:44:50 +0600

    api bugs resolved

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const catagories = require("./public/categories.json");
+const categories = require("./public/categories.json");
 const courses = require("./public/courses.json");
 
 app.use(cors());
 
-app.get("/categories", (req, res, next) => {
-  res.send(catagories);
+app.get("/category", (req, res, next) => {
+  res.send(categories);
 });
 
 app.get("/category/:id", (req, res, next) => {
-  const id = req.params.id;
-  const courses = courses.filter((course) => course.category_id === id);
+  const id = req.params.id * 1;
+  const category = categories.filter((course) => course.id === id);
+  res.send(category);
+});
+
+app.get("/course", (req, res) => {
   res.send(courses);
 });
 
-app.get("/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.get("/courses/:id", (req, res, next) => {
-  const id = req.params.id;
+app.get("/course/:id", (req, res, next) => {
+  const id = req.params.id * 1;
   const course = courses.filter((course) => course.id === id);
-  send(course);
+  res.send(course);
 });
 
 const PORT = 5000;
