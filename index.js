077bee26afff4b20cd20dceed8e37198f421ed9d
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-26 16:24:02 +0600

    categories api added

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const courses = require("./public/courses.json");
 
 app.use(cors());
 
+app.get("/categories", (req, res, next) => {
+  res.send(catagories);
+});
 
-app.get('/', (req, res, next)=> {
-
-})
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const courses = courses.filter((course) => course.category_id === id);
+  res.send(courses);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
